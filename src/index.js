c1fa6e2a6a536453d70314243c8a8414cb4389d4
Anatoly Kuzmin <tolaykuzay@gmail.com>
2020-01-08 18:17:08 +0300

    oh bugfix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+
+// import { Provider } from "react-redux"
+// import { createStore } from "redux"
+// import rootReducer from "./reducrers/index"
+
+// const store = createStore(rootReducer)
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>, document.getElementById('root')
+// );
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 import { Provider } from "react-redux"
 import { createStore } from "redux"
 import rootReducer from "./reducrers/index"
 
 const store = createStore(rootReducer)
-
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={store}>>  
     <App />
   </Provider>, document.getElementById('root')
 );
+
