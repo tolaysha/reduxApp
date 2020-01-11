8d6bceecfd4ebc617d44a8714f8a1127c1e3d78a
Anatoly Kuzmin <tolaykuzay@gmail.com>
2020-01-11 09:07:44 +0300

    work change name with bad practics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { setUser } from './reducrers/info'
 import { connect } from "react-redux"
-
+function handleClick(e) {
+  debugger; // TOFIX
+}
 class App extends Component {
-  render() {
+  debugger;
+  render() { // TOFIX
     return (
-          <h2>Welcome to React {this.props.user}</h2>
+          <Fragment>
+            <h2>Welcome to React {this.props.user}</h2>
+            <button onClick={()=>{debugger;this.props.onClick()}}>change store</button> 
+          </Fragment>
     );
   }
 }
@@ -15,8 +22,11 @@ class App extends Component {
 function mapStateToProps(state) {
   debugger;
   return {
-    user: state.userInfo.user
+    user: state.userInfo[0].user
   }
 }
+const mapDispatchToProps = (dispatch) => ({
+  onClick: () => {debugger;dispatch(setUser("new user"))}  // TOFIX
+})
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/reducrers/info.js
+++ src/reducrers/info.js
@@ -1,7 +1,23 @@
 const initialState = {
+    type: "INIT",
     user: "unknown user"
 }
 
-export default function userinfo(state = initialState){
-    return state
+export const setUser = user => ({
+    type: 'SET_USER',
+    user: user
+  })
+
+export default function userinfo(state = [{user:"unknown user"}], action){
+    switch (action.type) {
+        case 'SET_USER':
+            debugger;
+            return [
+                {
+                  user: action.user
+                }
+              ]
+        default:
+          return state
+      }
 }
